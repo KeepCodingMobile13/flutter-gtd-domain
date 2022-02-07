9939db34cbd9a044fb003ffc2c0b70b50323ecca
Fernando Rodríguez <frr@keepcoding.io>
2022-02-07 21:40:03 +0100

    initial version

---
+++ lib/gtd_domain.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library gtd_domain;
+
+export 'src/gtd_domain_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
